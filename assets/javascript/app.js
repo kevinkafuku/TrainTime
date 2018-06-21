c033273353b244faa4f8fd9bbaa4b6d7d3a88d78
Kevin Kafuku <kevkafuku@gmail.com>
2018-06-20 23:34:39 -0400

    trying to figure out database issue, small progress made

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,7 +3,7 @@ var config = {
     authDomain: "train-times-c4c4f.firebaseapp.com",
     databaseURL: "https://train-times-c4c4f.firebaseio.com",
     projectId: "train-times-c4c4f",
-    storageBucket: "",
+    storageBucket: "train-times-c4c4f.appspot.com",
     messagingSenderId: "994111517805"
   };
   firebase.initializeApp(config);
@@ -12,8 +12,10 @@ var config = {
 
   var name = "";
   var destination = "";
+  var givenTrainTime = "";
   var firstTrainTime = "";
   var frequency = "";
+
 
 
   $("#add-train-btn").on("click", function(event){
@@ -21,7 +23,9 @@ var config = {
 
       name = $("#train-name-input").val().trim();
       destination = $("#destination-input").val().trim();
-      firstTrainTime = $("#first-train-time-input").val().trim();
+      firstTrainTime = $("#first-time-input").val().trim();
+
+      
       frequency = $("#frequency-input").val().trim();
 
       database.ref().push({
@@ -33,14 +37,35 @@ var config = {
     });
     
     database.ref().on("child_added", function(childSnapshot){
+
+      var currentTrainTime = childSnapshot.val().firstTrainTime;
+      var timeFormat = "HH:mm";
+      var convertedTime = moment(currentTrainTime, timeFormat);
+      var displayedTime = moment(convertedTime).format("hh:mm A");
+
+      var frequencyRate = childSnapshot.val().frequency;
+
+      var firstTimeConverted = moment(currentTrainTime, "HH:mm").subtract(1, "years");
+      var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+      var tRemainder = diffTime % frequencyRate;
+      var tMinutesTillTrain = frequencyRate - tRemainder;
+
+
         console.log(childSnapshot.val().name);
         console.log(childSnapshot.val().destination);
         console.log(childSnapshot.val().firstTrainTime);
         console.log(childSnapshot.val().frequency);
 
+        // minutes away from firstTrainTime
+
+        // firstTrainTime - current time = ??
+
+        
+
         $("#train-table").append("<tr> <td> " + childSnapshot.val().name +
         " </td> <td> " + childSnapshot.val().destination +
         " </td> <td> " + childSnapshot.val().frequency +
-        " </td> <td> " + "THIS" + " </td> </tr>");
-    })
+        " </td> <td> " + displayedTime + " </td> <td> " + tMinutesTillTrain + " </td> </tr> "
+    );
   
+  });
